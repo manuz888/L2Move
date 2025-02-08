686229f31a6d20eb7f9835e6d1496848af1aac5c
Manuele Nerucci <nerucci.manuele@gmail.com>
2025-02-08 12:14:44 +0100

    feat: working on...

--- Source/LiveToMoveUI/Views/MainWindow.axaml.cs
+++ Source/LiveToMoveUI/Views/MainWindow.axaml.cs
@@ -1,7 +1,9 @@
-using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 using Avalonia.Input;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace LiveToMoveUI.Views;
 
@@ -20,6 +22,31 @@ public partial class MainWindow : Window
         e.DragEffects &= DragDropEffects.Copy;
     }
 
+    private async void OnOkClicked(object? sender, RoutedEventArgs e)
+    {
+        var processText = processButton.Content;
+        this.IsEnabled = false;
+
+        var cts = new CancellationTokenSource();
+        _ = this.AnimateButtonText(processButton, "Processing", cts.Token);
+        
+        try
+        {
+            // Simula un'operazione lunga
+            for (int i = 0; i <= 100; i += 10)
+            {
+                await Task.Delay(500); // Simula un lavoro lungo
+            }
+        }
+        finally
+        {
+            await cts.CancelAsync();
+
+            processButton.Content = processText;
+            this.IsEnabled = true;
+        }
+    }
+    
     private void Drop(object? sender, DragEventArgs e)
     {
         e.DragEffects &= DragDropEffects.Copy;
@@ -27,6 +54,21 @@ public partial class MainWindow : Window
         if (e.Data.Contains(DataFormats.Files))
         {
             // DropState.Text = e.Data.GetFiles()?.FirstOrDefault()?.Path.ToString();
+        }
+    }
+    
+    private async Task AnimateButtonText(Button button, string text, CancellationToken token)
+    {
+        var baseText = text;
+        string[] states = [".", "..", "..."];
+        var index = 0;
+
+        while (!token.IsCancellationRequested)
+        {
+            button.Content = baseText + states[index];
+            index = (index + 1) % states.Length;
+            
+            await Task.Delay(250, token); 
         }
     }
 }
