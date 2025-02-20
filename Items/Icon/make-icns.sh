ef31d07bc85c22df9aa3d6c333f36cd1f416c721
Manuele Nerucci <nerucci.manuele@gmail.com>
2025-02-20 08:50:34 +0100

    feat: improved make-icns

--- Items/Icon/make-icns.sh
+++ Items/Icon/make-icns.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-# TODO: to improve
+ICONSET_NAME=AppIcon.iconset
+if [ ! -d "$ICONSET_NAME" ]; then
+    mkdir $ICONSET_NAME
+fi
 
 # Create icons for various size
-magick icon_1024.png -resize 16x16 AppIcon.appiconset/icon_16x16.png
-magick icon_1024.png -resize 32x32 AppIcon.appiconset/icon_16x16@2x.png
-magick icon_1024.png -resize 32x32 AppIcon.appiconset/icon_32x32.png
-magick icon_1024.png -resize 64x64 AppIcon.appiconset/icon_32x32@2x.png
-magick icon_1024.png -resize 128x128 AppIcon.appiconset/icon_128x128.png
-magick icon_1024.png -resize 256x256 AppIcon.appiconset/icon_128x128@2x.png
-magick icon_1024.png -resize 256x256 AppIcon.appiconset/icon_256x256.png
-magick icon_1024.png -resize 512x512 AppIcon.appiconset/icon_256x256@2x.png
-magick icon_1024.png -resize 512x512 AppIcon.appiconset/icon_512x512.png
-magick icon_1024.png -resize 1024x1024 AppIcon.appiconset/icon_512x512@2x.png
+magick icon_1024.png -resize 16x16 $ICONSET_NAME/icon_16x16.png
+magick icon_1024.png -resize 32x32 $ICONSET_NAME/icon_16x16@2x.png
+magick icon_1024.png -resize 32x32 $ICONSET_NAME/icon_32x32.png
+magick icon_1024.png -resize 64x64 $ICONSET_NAME/icon_32x32@2x.png
+magick icon_1024.png -resize 128x128 $ICONSET_NAME/icon_128x128.png
+magick icon_1024.png -resize 256x256 $ICONSET_NAME/icon_128x128@2x.png
+magick icon_1024.png -resize 256x256 $ICONSET_NAME/icon_256x256.png
+magick icon_1024.png -resize 512x512 $ICONSET_NAME/icon_256x256@2x.png
+magick icon_1024.png -resize 512x512 $ICONSET_NAME/icon_512x512.png
+magick icon_1024.png -resize 1024x1024 $ICONSET_NAME/icon_512x512@2x.png
 
 # Create json content for iconset
-cat > AppIcon.appiconset/Contents.json <<EOL
+cat > $ICONSET_NAME/Contents.json <<EOL
 {
   "images": [
     {
@@ -87,4 +90,4 @@ cat > AppIcon.appiconset/Contents.json <<EOL
 EOL
 
 # Make icns file from iconset
-iconutil -c icns AppIcon.iconset
+iconutil -c icns $ICONSET_NAME
